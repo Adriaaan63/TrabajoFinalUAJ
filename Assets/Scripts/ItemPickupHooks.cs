e40005bed452359ed7f373e4a7979439026c219d
[MarcosPRC] <marcopan@ucm.es>
2026-05-17 00:01:39 +0200

    arreglos

--- Assets/Scripts/ItemPickupHooks.cs
+++ Assets/Scripts/ItemPickupHooks.cs
@@ -1,13 +1,27 @@
 using UnityEngine;
+using Opsive.Shared.Events;
 
 public class ItemPickupHooks : MonoBehaviour
 {
     [Header("Tipo de item")]
     public string itemType = "Health"; // "Health", "Weapon" o "Ammo"
 
-    void OnDisable()
+    void Start()
     {
-        if (Tracker.Instance == null || !Tracker.Instance.IsReady) return;
-        Tracker.Instance.TrackEvent(new Item_Picked(itemType));
+        EventHandler.RegisterEvent<GameObject>(gameObject, "OnItemPickup", OnItemPickedUp);
+    }
+
+    void OnDestroy()
+    {
+        EventHandler.UnregisterEvent<GameObject>(gameObject, "OnItemPickup", OnItemPickedUp);
+    }
+
+    private void OnItemPickedUp(GameObject picker)
+    {
+        if (!Tracker.Instance.IsReady) return;
+        if (picker != null && picker.CompareTag("Player"))
+        {
+            Tracker.Instance.TrackEvent(new Item_Picked(itemType));
+        }
     }
 }
