971585cb39581a80e3ab949b00f7d2b7cb57b68f
Adriaan63 <Adcast07@ucm.es>
2026-05-16 11:34:07 +0200

    Cimientos de la web, Inicialización y Configuración Base

---
+++ Assets/Docker/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-cyan-400">
+        FPS Telemetry Inicializado 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
